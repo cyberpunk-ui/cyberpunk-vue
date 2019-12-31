ed3bfa4f536e0848a1a7be433c41b41939566fdd
EvanBell <876895046@qq.com>
2019-12-31 11:03:32 +0800

    add button style & global style

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,15 +1,37 @@
 <template>
-  <button class="c-button">测试按钮</button>
+  <button class="c-button">
+    <slot />
+  </button>
 </template>
 
 <script>
   export default {
-    name: "c-button"
+    name: "c-button",
+    props: {
+       icon: {
+         type: String,
+       }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  @import '../../theme.scss';
+
   .c-button {
-    background-color: red;
+    padding: $button-padding;
+    font-size: $base-font-size;
+    color: $black-color;
+    background-color: $primary-color;
+    border: none;
+    outline: 2px solid $primary-color;
+    transition: all 0.3s;
+    letter-spacing: $button-text-space;
+    &:focus {
+      outline: 2px solid $secondary-color;
+    }
+    &:hover {
+      background-color: darken($primary-color, 8%);
+    }
   }
 </style>
