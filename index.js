aa9f36cd8d429fad759120f4370251e8c40585e7
EvanBell <876895046@qq.com>
2020-01-02 09:31:26 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import Button from './src/components/button';
+import ButtonGroup from './src/components/button-group';
+import Icon from './src/components/icon';
+
+
+export {
+  Button,
+  ButtonGroup,
+  Icon,
+};
