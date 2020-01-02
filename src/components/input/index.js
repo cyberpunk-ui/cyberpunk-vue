8c78fdbb6ca5b160f1b029ee9d33b68c0b06b536
EvanBell <876895046@qq.com>
2020-01-02 11:52:52 +0800

    add input component & add .travis.yml config

---
+++ src/components/input/index.js
@@ -0,0 +1,8 @@
+import CInput from './input';
+
+/* istanbul ignore next */
+CInput.install = function(Vue) {
+  Vue.component(CInput.name, CInput);
+};
+
+export default CInput;
---
+++ src/components/input/input.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "c-input"
+  }
+</script>
+
+<style scoped>
+
+</style>
