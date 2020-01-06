36c30299deadafdd707914e6b3ebd8547f9126a4
EvanBell <876895046@qq.com>
2020-01-06 16:22:20 +0800

    edit row.test.js

--- test/row.test.js
+++ test/row.test.js
@@ -7,48 +7,46 @@ Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
 describe('Row', () => {
-  it('create input.', function () {
+  it('create Row.', function () {
     expect(Row).to.be.ok;
   });
-  describe('[props]', function () {
-    it('gutter attribute.', () => {
-      Vue.component('c-row', Row)
-      Vue.component('c-col', Col)
-      const div = document.createElement('div');
-      document.body.append(div);
-      div.innerHTML = `
+  it('gutter attribute.', () => {
+    Vue.component('c-row', Row);
+    Vue.component('c-col', Col);
+    const div = document.createElement('div');
+    document.body.append(div);
+    div.innerHTML = `
         <c-row gutter="20">
           <c-col span="12"></c-col>
           <c-col span="12"></c-col>
         </c-row>
       `;
-      const vm = new Vue({
-        el: div
-      });
-      setTimeout(() => {
-        const row = vm.$el.querySelector('.c-row');
-        expect(getComputedStyle(row).marginLeft).to.eq('-10px');
-        expect(getComputedStyle(row).marginRight).to.eq('-10px');
-        const col = vm.$el.querySelectorAll('.c-col');
-        expect(getComputedStyle(col[0]).paddingLeft).to.eq('10px');
-        expect(getComputedStyle(col[0]).paddingRight).to.eq('10px');
-      });
+    const vm = new Vue({
+      el: div
     });
-    it('align attribute.', () => {
-      const div = document.createElement('div');
-      document.body.append(div);
-      const rowConstructor = Vue.extend(Row);
-      const row = new rowConstructor({
-        propsData: {
-          align: 'right'
-        }
-      }).$mount(div);
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.c-row');
+      expect(getComputedStyle(row).marginLeft).to.eq('-10px');
+      expect(getComputedStyle(row).marginRight).to.eq('-10px');
+      const col = vm.$el.querySelectorAll('.c-col');
+      expect(getComputedStyle(col[0]).paddingLeft).to.eq('10px');
+      expect(getComputedStyle(col[0]).paddingRight).to.eq('10px');
+    });
+  });
+  it('align attribute.', () => {
+    const div = document.createElement('div');
+    document.body.append(div);
+    const rowConstructor = Vue.extend(Row);
+    const row = new rowConstructor({
+      propsData: {
+        align: 'right'
+      }
+    }).$mount(div);
 
-      const rowElement = row.$el;
-      expect(getComputedStyle(rowElement).justifyContent).to.equal('flex-end');
-      div.remove();
-      row.$el.remove();
-      row.$destroy();
-    });
+    const rowElement = row.$el;
+    expect(getComputedStyle(rowElement).justifyContent).to.equal('flex-end');
+    div.remove();
+    row.$el.remove();
+    row.$destroy();
   });
 });
