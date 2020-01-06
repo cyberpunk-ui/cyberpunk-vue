e7cfd5741157b532390947660fe58b57d03ac4e7
EvanBell <876895046@qq.com>
2020-01-06 17:38:03 +0800

    add Layout demo.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import CButtonGroup from "./components/button-group";
 import CInput from "./components/input";
 import CRow from "./components/row";
 import CCol from "./components/col";
+import CAside from "./components/aside";
+import CContainer from "./components/container";
+import CMain from "./components/main";
+import CHeader from "./components/header";
+import CFooter from "./components/footer";
 
 Vue.use(CButton);
 Vue.use(CIcon);
@@ -12,6 +17,11 @@ Vue.use(CButtonGroup);
 Vue.use(CInput);
 Vue.use(CRow);
 Vue.use(CCol);
+Vue.use(CContainer);
+Vue.use(CAside);
+Vue.use(CHeader);
+Vue.use(CMain);
+Vue.use(CFooter);
 
 
 new Vue({
