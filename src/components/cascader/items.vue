3f7c41557e3714152458f00ea5b972960259f745
EvanBell <876895046@qq.com>
2020-02-11 13:31:24 +0800

    add cascader basic function.

---
+++ src/components/cascader/items.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="c-cascader-items">
+    <div class="left">
+      <div class="item" v-for="item in items" @click="leftSelected = item">
+        {{item.name}}
+      </div>
+    </div>
+    <div class="right" v-if="rightItems">
+      <c-cascader-items :items="rightItems"></c-cascader-items>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CCascaderItems",
+    props: {
+      items: {
+        type: Array,
+      }
+    },
+    data(){
+      return {
+        leftSelected: null,
+      }
+    },
+    computed: {
+      rightItems(){
+        if (this.leftSelected && this.leftSelected.children !== null) {
+          return this.leftSelected.children;
+        } else {
+          return null;
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../style/var';
+
+  .c-cascader-items {
+    display: flex;
+    color: white;
+    background-color: #fff;
+    z-index: 9;
+    position: relative;
+    .left{
+      width: 80px;
+
+      .item {
+        background-color: #fff;
+        color: $black-color;
+        padding: 0.5em 0.5em;
+        border-bottom: 1px solid $grey-color;
+        &:hover {
+          background-color: $primary-dark-color;
+        }
+      }
+    }
+  }
+</style>
--- src/components/cascader/main.vue
+++ src/components/cascader/main.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="c-cascader">
-    <slot></slot>
+    <div class="trigger" @click="visible = !visible">
+      <slot></slot>
+    </div>
+    <div class="popover" v-if="visible">
+      <cascader-items :items="source"></cascader-items>
+    </div>
   </div>
 </template>
 
 <script>
+  import cascaderItems from './items';
   export default {
     name: "CCascader",
+    components: {
+      'cascader-items': cascaderItems
+    },
     props: {
-
+      source: {
+        type: Array,
+      }
+    },
+    data() {
+      return {
+        visible: false
+      }
     },
   }
 </script>
@@ -17,6 +33,9 @@
   @import '../../style/var';
 
   .c-cascader {
-
+    position: relative;
+    .popover {
+      position: absolute;
+    }
   }
 </style>
