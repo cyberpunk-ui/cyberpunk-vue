b314fbf9aaaea061e092b79c21f22c478e173c9f
EvanBell <876895046@qq.com>
2020-03-04 23:48:23 +0800

    feat(upload):fix loading status error caused by duplicate id.

--- src/components/upload/main.vue
+++ src/components/upload/main.vue
@@ -13,13 +13,7 @@
         <span class="content" :class="{[file.status]:true}">
           <c-icon v-if="file.status === 'success'" type="success"></c-icon>
           <c-icon v-if="file.status === 'error'" type="reeor"></c-icon>
-          <span v-if="file.status === 'pending'" class="loading">
-            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-              <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-                <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/>
-              </path>
-            </svg>
-          </span>
+          <c-icon v-if="file.status === 'pending'" type="loading" class="loading"></c-icon>
           <a :href="file.url" :title="file.name">{{file.name}}</a>
         </span>
         <span class="close" @click="onRemoveFile(file)"><c-icon type="close"></c-icon></span>
@@ -91,7 +85,8 @@
         const rawFiles = Array.from(files)
         let file = rawFiles.map((rawFile, index) => {
           let {name, type, size} = rawFile
-          return {id: index, name, type, size, status: 'pending'}
+          let id = this.fileList.map(v => v.id).indexOf(index) >= 0 ? this.fileList.length : index
+          return {id, name, type, size, status: 'pending'}
         })
         this.$emit('update:fileList', [...this.fileList, ...file])
       },
@@ -100,11 +95,12 @@
         for (let i = 0; i < rawFiles.length; i++) {
           const formData = new FormData()
           formData.append(this.name, rawFiles[i])
+          const id = this.fileList.map(v => v.id).indexOf(i) >= 0 ? this.fileList.length : i
           this.ajax(formData, (response) => {
             this.$emit('onchange', response)
-            this.updateFileStatus(i, 'success')
+            this.updateFileStatus(id, 'success')
           },() => {
-            this.updateFileStatus(i,'error')
+            this.updateFileStatus(id,'error')
           })
         }
       },
@@ -168,11 +164,7 @@
           white-space: nowrap;
           vertical-align: middle;
           .loading{
-            vertical-align: middle;
-          }
-          svg path,
-          svg rect{
-            fill: $secondary-color;
+            color: $secondary-color;
           }
           /*&.success {*/
           /*}*/
