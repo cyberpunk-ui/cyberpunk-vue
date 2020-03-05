4e8c4ad1f8bbb1abe986399dcb3414159c83bfb7
EvanBell <876895046@qq.com>
2020-03-05 13:15:47 +0800

    test(message):fix row gutter & align unit test.

--- tests/unit/row.spec.js
+++ tests/unit/row.spec.js
@@ -1,49 +1,45 @@
-import { expect } from "chai";
+import Vue from "vue"
+import {expect} from "chai"
 import { mount } from "@vue/test-utils";
 import Row from "@/components/row/row";
-import Col from "@/components/col/col";
+import CCol from "@/components/col";
+
+Vue.use(CCol);
+const template = `
+  <c-col span="12"></c-col>
+  <c-col span="12"></c-col>
+`
 
 describe("[Row]", () => {
   it("create row component.", function() {
     expect(Row).to.be.ok;
+    expect(CCol).to.be.ok;
   });
-  xit("set gutter attribute.", () => {
-    Vue.component("c-row", Row);
-    Vue.component("c-col", Col);
-    const div = document.createElement("div");
-    document.body.append(div);
-    div.innerHTML = `
-        <c-row gutter="20">
-          <c-col span="12"></c-col>
-          <c-col span="12"></c-col>
-        </c-row>
-      `;
-    const vm = new Vue({
-      el: div
-    });
-    setTimeout(() => {
-      const row = vm.$el.querySelector(".c-row");
-      expect(getComputedStyle(row).marginLeft).to.eq("-10px");
-      expect(getComputedStyle(row).marginRight).to.eq("-10px");
-      const col = vm.$el.querySelectorAll(".c-col");
-      expect(getComputedStyle(col[0]).paddingLeft).to.eq("10px");
-      expect(getComputedStyle(col[0]).paddingRight).to.eq("10px");
-    });
+  it("set gutter attribute.", () => {
+    const wrapper = mount(Row, {
+      attachToDocument: true,
+      slots: {
+        default: template,
+      },
+      propsData: {
+        gutter: 20
+      }
+    })
+    const row = wrapper.find(".c-row").element;
+    expect(getComputedStyle(row).marginLeft).to.eq("-10px");
+    expect(getComputedStyle(row).marginRight).to.eq("-10px");
   });
-  xit("set align attribute.", () => {
-    const div = document.createElement("div");
-    document.body.append(div);
-    const rowConstructor = Vue.extend(Row);
-    const row = new rowConstructor({
+  it("set align attribute.", () => {
+    const wrapper = mount(Row, {
+      attachToDocument: true,
+      slots: {
+        default: template,
+      },
       propsData: {
         align: "right"
       }
-    }).$mount(div);
-
-    const rowElement = row.$el;
-    expect(getComputedStyle(rowElement).justifyContent).to.equal("flex-end");
-    div.remove();
-    row.$el.remove();
-    row.$destroy();
+    })
+    const row = wrapper.find(".c-row").element;
+    expect(getComputedStyle(row).justifyContent).to.eq("flex-end");
   });
 });
