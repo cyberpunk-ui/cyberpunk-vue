f569fc849eae4d86edc266becd8edc6be6ee95ae
EvanBell <876895046@qq.com>
2020-03-06 18:23:34 +0800

    test(upload):add remove file test. remove all done.

--- tests/unit/upload.spec.js
+++ tests/unit/upload.spec.js
@@ -10,11 +10,10 @@ describe("[Upload]", () => {
   it("create upload component.", () => {
     expect(Upload).to.be.ok;
   });
-  it("test upload one file.", (done) => {
+  it("test upload one file.", () => {
     http.post = (url, options) => {
       setTimeout(()=>{
         options.success('{key: "a0cd70fb3da0e18426665712a946f7d2"}')
-        done()
       }, 200)
     }
     const wrapper = mount(Upload, {
@@ -49,11 +48,10 @@ describe("[Upload]", () => {
       expect(wrapper.find('.content').classes()).to.include('success')
     },210)
   });
-  it("test upload multiple file.", (done) => {
+  it("test upload multiple file.", () => {
     http.post = (url, options) => {
       setTimeout(()=>{
         options.success('{key: "a0cd70fb3da0e18426665712a946f7d2"}')
-        done()
       }, 200)
     }
     const wrapper = mount(Upload, {
@@ -98,5 +96,78 @@ describe("[Upload]", () => {
       expect(wrapper.findAll('.content').at(1).classes()).to.include('success')
       expect(wrapper.findAll('.content').at(2).classes()).to.include('success')
     },210)
+  });
+  it("test remove file.", (done) => {
+    http.post = (url, options) => {
+      setTimeout(()=>{
+        options.success('{key: "a0cd70fb3da0e18426665712a946f7d2"}')
+        done()
+      }, 100)
+    }
+    const wrapper = mount(Upload, {
+      propsData: {
+        name: 'file',
+        action: '/xxx',
+        fileList: [],
+      },
+      slots: {
+        default: `<button id="test">upload</button>`
+      },
+      listeners: {
+        'update:fileList': (fileList)=> {
+          wrapper.setProps({fileList})
+        },
+      }
+    })
+    wrapper.find('#test').trigger('click')
+
+    const inputWrapper = wrapper.find('input[type="file"]')
+    const input = inputWrapper.element
+    let file = new File(['test'], 'xxx.txt')
+    const data = new DataTransfer()
+    data.items.add(file)
+    input.files = data.files
+    inputWrapper.trigger('change')
+    setTimeout(()=> {
+      expect(wrapper.find('.content').classes()).to.include('success')
+      wrapper.find('.close').trigger('click')
+      expect(wrapper.find('ol').element.children.length).to.eq(0)
+    },110)
+  });
+  it("set accept attribute.", (done) => {
+    http.post = (url, options) => {
+      setTimeout(()=>{
+        options.success('{key: "a0cd70fb3da0e18426665712a946f7d2"}')
+        done()
+      }, 100)
+    }
+    const wrapper = mount(Upload, {
+      propsData: {
+        name: 'file',
+        action: '/xxx',
+        fileList: [],
+        accept: 'text/plain'
+      },
+      slots: {
+        default: `<button id="test">upload</button>`
+      },
+      listeners: {
+        'update:fileList': (fileList)=> {
+          wrapper.setProps({fileList})
+        },
+      }
+    })
+    wrapper.find('#test').trigger('click')
+
+    const inputWrapper = wrapper.find('input[type="file"]')
+    const input = inputWrapper.element
+    let file = new File(['test'], 'xxx.txt', {type: 'text/plain'})
+    const data = new DataTransfer()
+    data.items.add(file)
+    input.files = data.files
+    inputWrapper.trigger('change')
+    setTimeout(()=> {
+      expect(wrapper.find('.content').classes()).to.include('success')
+    },110)
   });
 });
