9798c7c44d39f44732a1c8c86fb4f95937a568ff
EvanBell <876895046@qq.com>
2020-03-30 20:44:40 +0800

    fix(cascader): fix cascader item icon not centered.

--- src/components/cascader/items.vue
+++ src/components/cascader/items.vue
@@ -30,108 +30,105 @@
 </template>
 
 <script>
-import Icon from "../icon/icon";
-export default {
-  name: "CCascaderItems",
-  components: { "c-icon": Icon },
-  props: {
-    items: {
-      type: Array
+  import Icon from "../icon/icon";
+  export default {
+    name: "CCascaderItems",
+    components: { "c-icon": Icon },
+    props: {
+      items: {
+        type: Array
+      },
+      selected: {
+        type: Array,
+        default: () => []
+      },
+      level: {
+        type: Number,
+        default: 0
+      },
+      loadData: {
+        type: Function,
+      },
+      loadingItem: {
+        type: Object,
+        default: () => ({})
+      },
+      close: {
+        type: Function,
+      },
     },
-    selected: {
-      type: Array,
-      default: () => []
-    },
-    level: {
-      type: Number,
-      default: 0
-    },
-    loadData: {
-      type: Function,
-    },
-    loadingItem: {
-      type: Object,
-      default: () => ({})
-    },
-    close: {
-      type: Function,
-    },
-  },
-  computed: {
-    /* eslint-disable */
-    rightItems () {
-      if (this.selected[this.level]) {
-        let selected = this.items.filter((item) => item.name === this.selected[this.level].name);
-        if (selected && selected[0].children && selected[0].children.length > 0) {
-          return selected[0].children
+    computed: {
+      /* eslint-disable */
+      rightItems () {
+        if (this.selected[this.level]) {
+          let selected = this.items.filter((item) => item.name === this.selected[this.level].name);
+          if (selected && selected[0].children && selected[0].children.length > 0) {
+            return selected[0].children
+          }
         }
+      },
+    },
+    methods: {
+      onClickItem(item) {
+        const copySelected = JSON.parse(JSON.stringify(this.selected));
+        copySelected[this.level] = item;
+        copySelected.splice(this.level + 1);
+        this.$emit('update:selected', copySelected);
+        const lastItem = copySelected && copySelected[copySelected.length - 1];
+        if (lastItem && (this.loadData ? lastItem.isLeaf : !lastItem.children)) {
+          this.close();
+        }
+      },
+      onUpdateSelected(newSelected){
+        this.$emit('update:selected', newSelected);
+      },
+      rightArrowVisible(item){
+        return this.loadData ? !item.isLeaf : item.children;
       }
-    },
-  },
-  methods: {
-    onClickItem(item) {
-      const copySelected = JSON.parse(JSON.stringify(this.selected));
-      copySelected[this.level] = item;
-      copySelected.splice(this.level + 1);
-      this.$emit('update:selected', copySelected);
-      const lastItem = copySelected && copySelected[copySelected.length - 1];
-      if (lastItem && (this.loadData ? lastItem.isLeaf : !lastItem.children)) {
-        this.close();
-      }
-    },
-    onUpdateSelected(newSelected){
-      this.$emit('update:selected', newSelected);
-    },
-    rightArrowVisible(item){
-      return this.loadData ? !item.isLeaf : item.children;
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "../../style/var";
-@import "../../style/animation";
+  @import "../../style/var";
+  @import "../../style/animation";
 
-.c-cascader-items {
-  display: flex;
-  color: white;
-  z-index: 9;
-  position: relative;
-  background-color: $grey-color;
-  height: 200px;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
-  overflow: hidden;
-  white-space: nowrap;
-  .left {
-    padding-top: 0.3em;
-    overflow: auto;
-    min-width: 120px;
-    .item {
-      position: relative;
-      font-size: 14px;
-      color: $white-color;
-      padding: 0.5em 0.8em;
-      transition: all 0.2s;
-      cursor: pointer;
-      &.active,
-      &:hover {
-        background-color: $primary-color;
-        color: $black-color;
-      }
-      .icon {
-        position: absolute;
-        right: 6px;
-        top: 3px;
-      }
-      .loading {
-        animation: spin 1.2s infinite linear;
+  .c-cascader-items {
+    display: flex;
+    color: white;
+    z-index: 9;
+    position: relative;
+    background-color: $grey-color;
+    height: 200px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+    white-space: nowrap;
+    .left {
+      padding-top: 0.3em;
+      overflow: auto;
+      min-width: 120px;
+      .item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 14px;
+        color: $white-color;
+        padding: 0.5em 0.8em;
+        transition: all 0.2s;
+        cursor: pointer;
+        &.active,
+        &:hover {
+          background-color: $primary-color;
+          color: $black-color;
+        }
+        .loading {
+          animation: spin 1.2s infinite linear;
+        }
       }
     }
+    .right {
+      overflow: auto;
+      border-left: 1px solid $grey-light-color;
+    }
   }
-  .right {
-    overflow: auto;
-    border-left: 1px solid $grey-light-color;
-  }
-}
 </style>
